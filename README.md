a38bc806c41382425a33704ce73f929e7ad139c5
astroxvim <valerosojamesmartin@gmail.com>
2025-03-06 16:45:35 +0900

    👋initial commit for astroxvim

---
+++ README.md
@@ -0,0 +1,3 @@
+# My personal website
+
+This website is made with Astro :)
---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astroxvim.github.io',
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  server: {
+    host: "127.0.0.1"
+  }
+});
